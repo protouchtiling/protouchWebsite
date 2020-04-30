5fffbc68cd3e70a8f4717fe7bbce488a03f0bfd6
Protouch <protouch.tiles@gmail.com>
2020-04-29 22:13:07 -0700

    Updates

---
+++ precache-manifest.3dc7fdce501e9481166f35dac11eb937.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15720cac76bdb7f4e26211e6506f9ccc",
+    "url": "/theprotouchtiling.com/index.html"
+  },
+  {
+    "revision": "e74270c5a87e426a3cb6",
+    "url": "/theprotouchtiling.com/static/css/main.f3126295.chunk.css"
+  },
+  {
+    "revision": "936e59fed35e6edc3be4",
+    "url": "/theprotouchtiling.com/static/js/2.038fb0fe.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/theprotouchtiling.com/static/js/2.038fb0fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e74270c5a87e426a3cb6",
+    "url": "/theprotouchtiling.com/static/js/main.718cfd92.chunk.js"
+  },
+  {
+    "revision": "2505f84d716b60d60dfd",
+    "url": "/theprotouchtiling.com/static/js/runtime-main.082e86a7.js"
+  },
+  {
+    "revision": "785d13c2c7550f0a321cff84dd125899",
+    "url": "/theprotouchtiling.com/static/media/backsplash.785d13c2.jpg"
+  },
+  {
+    "revision": "7c51e56863819d72e22b386202e46cc5",
+    "url": "/theprotouchtiling.com/static/media/backsplash.7c51e568.jpeg"
+  },
+  {
+    "revision": "1b5f3a1ead0af07dfa22d0c2ab8b08c1",
+    "url": "/theprotouchtiling.com/static/media/fireplace.1b5f3a1e.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/theprotouchtiling.com/static/media/map-image.f40ed5b1.png"
+  },
+  {
+    "revision": "3810a9ac1dda16ac87e01d097b56cf66",
+    "url": "/theprotouchtiling.com/static/media/renovation.3810a9ac.jpg"
+  },
+  {
+    "revision": "e4f0f2eba1821b8321e3bfe91e8fa16a",
+    "url": "/theprotouchtiling.com/static/media/tub.e4f0f2eb.jpg"
+  }
+]);
